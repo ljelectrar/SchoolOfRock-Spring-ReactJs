ce5d04e5aa2664057bb2447336c226169980b9ba
Leandro Junior Alves Dos Santos <ljelectrar@outlook.com>
2023-12-21 07:52:49 -0300

    Project created and initial test.

---
+++ src/main/java/com/ljelectrar/SchoolOfRock/SchoolOfRockApplication.java
@@ -0,0 +1,13 @@
+package com.ljelectrar.SchoolOfRock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolOfRockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolOfRockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ljelectrar/SchoolOfRock/SchoolOfRockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ljelectrar.SchoolOfRock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolOfRockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
